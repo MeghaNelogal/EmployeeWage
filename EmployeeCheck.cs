d498ef25632d0bbb2073a0f29874014a3da8d091
Your Name <you@example.com>
2022-07-29 01:20:40 +0530

    Using Switch Case Statement

--- EmployeeCheck.cs
+++ EmployeeCheck.cs
@@ -29,24 +29,37 @@ namespace EmployeeWage
 
             int empCheck  = random.Next(3);
 
-            if(empCheck== IS_FULL_TIME)
+            //if(empCheck== IS_FULL_TIME)
+            //{
+            //    Console.WriteLine("Employee is Present");
+            //    empHrs = 8;
+            //}
+            //else if(empCheck==IS_PART_TIME)
+            //{
+            //    Console.WriteLine("Employee is Part Time");
+            //    empHrs = 4;
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Employee is Absent");
+            //    empHrs = 0;
+            //}
+            switch(empCheck)
             {
-                Console.WriteLine("Employee is Present");
-                empHrs = 8;
-            }
-            else if(empCheck==IS_PART_TIME)
-            {
-                Console.WriteLine("Employee is Part Time");
-                empHrs = 4;
-            }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-                empHrs = 0;
+                case 1:
+                    Console.WriteLine("IS_FULL_TIME is ->");
+                    empHrs = 8;
+                    break;
+                    case 2:
+                    Console.WriteLine("IS_PART_TIME is ->");
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Emp Wage : " + empWage);
-
        }
 
     }
