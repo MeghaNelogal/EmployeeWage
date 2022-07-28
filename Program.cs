e8fb159680a5305d581a15df9f12a0b718315d10
Your Name <you@example.com>
2022-07-29 00:26:40 +0530

    Added UC1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
-﻿namespace Employeewage
+﻿using EmployeeWage;
+
+namespace Employeewage
 {
     class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welome To Employee Wage Program");
+            EmployeeCheck.CheckEmployeeAttendance();
 
         }
     }
