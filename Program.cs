e1c9322a1cfde7862b4108cbbd2561cb816a4e2c
Your Name <you@example.com>
2022-07-29 00:04:32 +0530

    Greating Message

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Employeewage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welome To Employee Wage Program");
+
+        }
+    }
+}
